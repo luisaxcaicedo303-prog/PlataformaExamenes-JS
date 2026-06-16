d8047c247ca8134bb5d54fe1a49e7b13fc486d69
luisaxcaicedo303 <luisaxcaicedo303@gmail.com>
2026-06-16 15:46:28 -0500

    fix: correcion del js

--- JS/login.js
+++ JS/login.js
@@ -1,10 +1,10 @@
-// login.js
+
 document.querySelector('form').addEventListener('submit', function(event) {
     const email = document.getElementById('username').value;
     const pass = document.getElementById('password').value;
 
     if (email === "" || pass === "") {
-        event.preventDefault(); // Evita que se envíe el formulario
+        event.preventDefault(); 
         alert("Por favor, completa todos los campos.");
     } else {
         console.log("Validación exitosa, enviando datos...");
